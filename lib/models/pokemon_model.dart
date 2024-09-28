24b6bcf8ef2e961c3f73b611ff53a13f58b15d5b
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-09-28 09:46:28 -0500

    model

---
+++ lib/models/pokemon_model.dart
@@ -0,0 +1,59 @@
+// To parse this JSON data, do
+//
+//     final pokemonModel = pokemonModelFromJson(jsonString);
+
+import 'dart:convert';
+
+PokemonModel pokemonModelFromJson(String str) =>
+    PokemonModel.fromJson(json.decode(str));
+
+String pokemonModelToJson(PokemonModel data) => json.encode(data.toJson());
+
+class PokemonModel {
+  int count;
+  String next;
+  dynamic previous;
+  List<Result> results;
+
+  PokemonModel({
+    required this.count,
+    required this.next,
+    required this.previous,
+    required this.results,
+  });
+
+  factory PokemonModel.fromJson(Map<String, dynamic> json) => PokemonModel(
+        count: json["count"],
+        next: json["next"],
+        previous: json["previous"],
+        results:
+            List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "count": count,
+        "next": next,
+        "previous": previous,
+        "results": List<dynamic>.from(results.map((x) => x.toJson())),
+      };
+}
+
+class Result {
+  String name;
+  String url;
+
+  Result({
+    required this.name,
+    required this.url,
+  });
+
+  factory Result.fromJson(Map<String, dynamic> json) => Result(
+        name: json["name"],
+        url: json["url"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+        "url": url,
+      };
+}
