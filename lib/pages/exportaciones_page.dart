d618f21f9f33c128140cd0c61dee7e75d6567ea9
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-09-28 12:25:36 -0500

    excel_

--- lib/pages/exportaciones_page.dart
+++ lib/pages/exportaciones_page.dart
@@ -6,6 +6,7 @@ import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:syncfusion_flutter_xlsio/xlsio.dart' as excel;
 
 class ExportacionesPage extends StatelessWidget {
   exportPDF() async {
@@ -68,6 +69,23 @@ class ExportacionesPage extends StatelessWidget {
     OpenFile.open(fileName);
   }
 
+  exportExcel() async {
+    final workbook = excel.Workbook();
+    final excel.Worksheet sheet = workbook.worksheets[0];
+    sheet.getRangeByName("A1").setText("HOLAAAAAAAA");
+    sheet.getRangeByIndex(2, 1).setText("HOLA 2");
+
+    final List<int> bytes = workbook.saveAsStream();
+    workbook.dispose();
+
+    final String path = (await getApplicationSupportDirectory()).path;
+    final String fileName = "$path/miExcel.xlsx";
+    final File file = File(fileName);
+    await file.writeAsBytes(bytes, flush: true);
+
+    OpenFile.open(fileName);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -81,6 +99,12 @@ class ExportacionesPage extends StatelessWidget {
                   exportPDF();
                 },
                 child: Text("Exportar a pdf"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  exportExcel();
+                },
+                child: Text("Exportar a excel"),
               ),
             ],
           ),
